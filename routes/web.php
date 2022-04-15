abcb5984bb49748cc6787536c3ca99240ab0dd34
b23922853 <70330313+b23922853@users.noreply.github.com>
2022-04-15 16:44:54 +0800

    GO

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\NewsController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [Controller::class, 'index']);
+
+// Route::get('/test', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/say', function () {
+//     return 'Hello world!';
+// });
+
+Route::get('/microsoft', [NewsController::class, 'index']);
